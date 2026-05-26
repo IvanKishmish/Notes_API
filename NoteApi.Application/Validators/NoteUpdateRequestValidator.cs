d5134737ee693a07a8ae0643d4ea47562c228876
IvanKishmish <ivanlitovchenco13@gmail.com>
2026-05-26 22:31:27 +0300

    Add: validators

---
+++ NoteApi.Application/Validators/NoteUpdateRequestValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+using NoteApi.Application.DTOs;
+
+namespace NoteApi.Application.Validators;
+
+public class NoteUpdateRequestValidator : AbstractValidator<NoteUpdateRequestDto>
+{
+    public NoteUpdateRequestValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title cannot be empty.")
+            .MaximumLength(100).WithMessage("Title cannot exceed 100 characters.");
+
+        RuleFor(x => x.Details)
+            .NotEmpty().WithMessage("Details cannot be empty.")
+            .MaximumLength(2000).WithMessage("Details cannot exceed 2000 characters.");
+    }
+}
